31dffedba9e6ccfedb4ed8a9ff656f8dfcb10408
Orangel Brito <orangeleando@gmail.com>
2022-06-12 13:30:19 -0500

    Añadí el index.html con links al resto de documentos

--- analisis.js
+++ analisis.js
@@ -1,4 +1,39 @@
-//Creamos un array con solo los salarios :
+//Helpers
+function esPar(numero) {
+    return (numero % 2 === 0);
+}
+
+
+function calcularMediaAritmetica(lista) {
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+// Calculadora
+//Ahora creamos una function que calcule la mediana de los salarios para cualquier país :
+
+function medianaSalarios(lista){
+    const mitad = parseInt(lista.length / 2);
+
+    if(esPar(lista.length)){
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad];
+
+        const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
+        return mediana;
+    } else {
+        const personaMitad = lista[mitad];
+        return personaMitad;
+    }
+}
+
+// Mediana General
 
 const salariosCol = colombia.map(
 
@@ -14,45 +49,21 @@ const salariosColSorted = salariosCol.sort(
     }
 );
 
-//Para calcular la mediana depende si el nro es par o impar, entonces se crea la función :
+    const medianaGeneralCol = medianaSalarios(salariosColSorted);
+    
+    // Mediana del top 10%
+    const spliceStart = (salariosColSorted.length * 90) / 100;
+    const spliceCount = salariosColSorted.length - spliceStart;
 
-
-function esPar(numero) {
-    return (numero % 2 === 0);
-}
-
-
-function calcularMediaAritmetica(lista) {
-
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento) {
-            return valorAcumulado + nuevoElemento;
-        }
+    const salariosColTop10 = salariosColSorted.splice(
+        spliceStart,
+        spliceCount,
     );
 
-    const promedioLista = sumaLista / lista.length;
+    const medianaTop10Col = medianaSalarios(salariosColTop10);
 
-    return promedioLista;
-}
-//Ahora creamos una function que calcule la mediana de los salarios para cualquier país :
 
-    function medianaSalarios(lista){
-        const mitad = parseInt(lista.length / 2);
-    
-        if(esPar(lista.length)){
-            const personaMitad1 = lista[mitad - 1];
-            const personaMitad2 = lista[mitad];
-    
-    
-            const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
-            return mediana;
-        } else {
-            const personaMitad = lista[mitad];
-            return personaMitad;
-        }
-    }
-    
-    
-    console.log(
-        medianaSalarios(salariosColSorted)
-    );
+    console.log({
+        medianaGeneralCol,
+        medianaTop10Col,
+    });
